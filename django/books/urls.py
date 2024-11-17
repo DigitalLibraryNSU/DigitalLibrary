d1c25c923b0279a0ab176d74b11922ea4a8f9931
Ilya Kislitsyn <i.kislitsyn@g.nsu.ru>
2024-11-17 16:07:05 +0700

    added django admin panel

--- django/books/urls.py
+++ django/books/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
 
 urlpatterns = [
     path('books/', views.BookListCreate.as_view()),
@@ -8,4 +9,5 @@ urlpatterns = [
     path('collections/<int:collection_id>/', views.CollectionDetailView.as_view(), name='get_collection_by_id'),
     path('bookByAuthor/<str:book_author>/', views.BookAuthorSearch.as_view(), name='book_by_author'),
     path('bookByTitle/<str:book_title>/', views.BookTitleSearch.as_view(), name='book_by_title'),
+    path('admin/', admin.site.urls),
 ]

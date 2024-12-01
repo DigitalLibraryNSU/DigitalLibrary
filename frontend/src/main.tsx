2abbc5e93db7750ddd260af037c896065d4de452
Marya Yukhnina <m.yukhnina@g.nsu.ru>
2024-12-01 08:23:30 +0700

    fix: adaptive, fonts

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,8 +4,10 @@ import App from './App.tsx'
 import './index.css'
 import './assets/Broadleaf-Regular.ttf'
 import './assets/Newake-Font-Demo.otf'
-
-
+import "./assets/Rubik-BoldItalic.ttf"
+import "./assets/Rubik-Italic.ttf"
+import "./assets/Rubik-Bold.ttf"
+import "./assets/Rubik-Regular.ttf"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- frontend/src/pages/CollectionsPage.tsx
+++ frontend/src/pages/CollectionsPage.tsx
@@ -1,12 +1,10 @@
 import { observer } from "mobx-react-lite";
-import { Col, Layout, Row } from "antd";
+import { Layout} from "antd";
 import { Header } from "../components/header.tsx";
 import WhiteCollectionCard from "../components/whiteCollectionCard.tsx";
 import BlackCollectionCard from "../components/blackCollectionCard.tsx";
 import { useStore } from "../Store/StoreContext.tsx";
 import { useEffect } from "react";
-
-const { Content } = Layout;
 
 interface Collection {
     name: string;
@@ -28,28 +26,36 @@ const CollectionsPage: React.FC = observer(() => {
     return (
         <Layout style={{ minHeight: "100vh" }}>
             <Header />
-            <Content style={{ padding: "0 48px", marginTop: "90px" }}>
-                <div style={{ minHeight: 280, padding: 24, borderRadius: 10 }}>
-                    <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-                        {collectionsStore.collections.map((collection: Collection) => (
-                            <Col
-                                xs={24}
-                                sm={24}
-                                md={12}
-                                lg={8}
-                                key={collection.id}
-                                style={{ marginTop: "20px", marginBottom: "10px" }}
-                            >
-                                {(collection.id / 2) % 2 === 0 ? (
-                                    <BlackCollectionCard title={collection.name} description={collection.description} collectionId={collection.documentId} />
-                                ) : (
-                                    <WhiteCollectionCard title={collection.name} description={collection.description} collectionId={collection.documentId} />
-                                )}
-                            </Col>
-                        ))}
-                    </Row>
+                <div
+                    style={{
+                        minHeight: 280,
+                        marginRight: "40px",
+                        marginLeft: "40px",
+                        marginTop: "90px",
+                        borderRadius: 10,
+                        display: "flex",
+                        flexWrap: "wrap",
+                        justifyContent: "space-between",
+                    }}
+                >
+                    {collectionsStore.collections.map((collection: Collection) =>
+                        (collection.id / 2) % 2 === 0 ? (
+                            <BlackCollectionCard
+                                key={collection.documentId}
+                                title={collection.name}
+                                description={collection.description}
+                                collectionId={collection.documentId}
+                            />
+                        ) : (
+                            <WhiteCollectionCard
+                                key={collection.documentId}
+                                title={collection.name}
+                                description={collection.description}
+                                collectionId={collection.documentId}
+                            />
+                        )
+                    )}
                 </div>
-            </Content>
         </Layout>
     );
 });
